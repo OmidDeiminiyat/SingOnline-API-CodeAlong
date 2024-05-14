8d9f1ed76d8ecdc031b7b903f7b8ecdf86e54d19
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-05-14 11:02:57 +0200

    supabase

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
-const app = express()
-dotenv.config();
+import { supabase } from './Config/supabase.config.js';
 
+dotenv.config();
 const port = process.env.PORT
+
+const app = express()
 
 app.get('/', (req, res) => {
 res.send('Hello World')
@@ -14,6 +16,36 @@ app.post('/', (req, res) => {
     res.send('Om os')
     
     })
+
+    app.get('/test', async (req, res) => {
+        const { data, error } = await supabase.from('songs').select('title, id')
+        if (error) { 
+           console.error(error)
+        } else { 
+           console.log(data)
+        }
+    })
+
+    app.get('/artists', async (reg, res) => {
+        const {data, error} = await supabase.from('artists').select('*')
+        if(error) {
+            console.error(error)
+        } else {
+            console.log(data)
+        }
+    })
+
+    app.get('/album', async (reg, res) => {
+        const {data, error} = await supabase.from('albums').select('title, image, artists(id, name)')
+        if(error) {
+            console.error(error)
+        } else {
+            console.log(data)
+        }
+    })
+
+    
+
 app.listen(port, () => {
     console.log(`Server runs on http://localhost:${port}`);
 })
