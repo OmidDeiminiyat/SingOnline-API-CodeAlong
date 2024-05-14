9b530e7276df5519c324bed5a00d0a3add58982f
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-05-14 08:33:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express'
 const app = express()
 const port = 3000
+
+app.get('/', (req, res) => {
+res.send('Hello World')
+console.log(reg.query);
+})
+
+app.post('/', (req, res) => {
+    res.send('Om os')
+    
+    })
 app.listen(port, () => {
     console.log(`Server runs on http://localhost:${port}`);
 })
