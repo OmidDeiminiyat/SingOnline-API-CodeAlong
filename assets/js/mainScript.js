45266701f6bb348f05698a898d706173e687d6c3
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-06-06 15:01:45 +0200

    done with design landing page

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -1,13 +1,20 @@
 const navigation = document.getElementById('navMobile');
 const navBut = document.getElementById('hiddenIcone');
 const nayBeOpen = document.getElementById('apearIcone');
+const main = document.getElementById('Main');
 function openNav() {
     navigation.style.display='block';
     navBut.style.display='none';
     nayBeOpen.style.display='block';
+    main.style.opacity ='0.2';
 }
 function closeNav() {
     navigation.style.display='none';
     navBut.style.display='block';
     nayBeOpen.style.display='none';
+    main.style.opacity ='1';
 }
+
+function changeImage(newImage) {
+    document.getElementById('imgLarge').src = newImage;
+  }
